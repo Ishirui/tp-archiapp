14e3989a055e98f2b2d0950370c3cad7d69879d0
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-07 14:41:45 +0100

    feat: Fonction applique

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ function fact(n) {
 }
 
 console.log(fact(5)); // 120
-console.log(fact(-1)); // Error: n must be non-negative
+// console.log(fact(-1)); // Error: n must be non-negative
+
+function applique(f, tab) {
+    res = []
+    tab.forEach(element => {
+        res.push(f(element))
+    });
+    return res;
+}
+
+console.log(applique(fact, [1, 2, 3, 4, 5])); // [1, 2, 6, 24, 120]
