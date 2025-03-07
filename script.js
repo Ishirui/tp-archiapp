ebfb2e6c5de38fecec646059a1805c21358ff7da
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-07 14:43:16 +0100

    feat: applique avec fonction anonyme

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ function applique(f, tab) {
 }
 
 console.log(applique(fact, [1, 2, 3, 4, 5])); // [1, 2, 6, 24, 120]
+
+// On peut passer une fonction anonyme comme n'importe quel autre objet
+// L'objet est instantié avant l'appel de applique (on évalue d'abord les arguments)
+console.log(applique(function(n) {return n+1}, [1, 2, 3, 4, 5])); // [2, 3, 4, 5, 6]
