4731b0ed5d1ff30d2df859477e92dd5ac632fb18
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-15 13:57:53 +0100

    feat: Routes microservice compteur basique

--- serveur/index.js
+++ serveur/index.js
@@ -14,13 +14,33 @@ app.use(function (req, res, next) {
 // Ici faut faire faire quelque chose à notre app...
 // On va mettre les "routes"  == les requêtes HTTP acceptéés par notre application.
 
-// app.get("/", function (req, res) {
-//   res.send("Hello");
-// });
+let cpt = 0;
 
-app.get("/test/*", function (req, res) {
-  let msg = decodeURIComponent(req.url.substr(6)); // Deprecated d'après TS ?
-  res.json({ msg: msg });
+app.get("/cpt/query", function (req, res) {
+  res.json(cpt);
+});
+
+app.post("/cpt/inc", function (req, res) {
+  // Un POST semble plus approprié qu'un GET (pas idempotent), un PATCH serait peut-être encore mieux
+
+  // Si la requête ne contient pas de paramètre, on incrémente de 1.
+  if (!req.query.v) {
+    cpt++;
+    res.json(cpt);
+    return;
+  }
+
+  // Sinon, on incrémente de la valeur passée en paramètre.
+  const val = parseInt(req.query.v);
+
+  // Si on ne peut pas décoder la valeur, le résultat sera NaN
+  if (isNaN(val)) {
+    res.json({ code: -1 }); // Un 400 Bad Request serait plus approprié
+    return;
+  }
+
+  cpt += val;
+  res.json({ code: 0 }); // Je mettrais plutôt un 200 OK pour faire une vraie API REST mais bon
 });
 
 app.listen(8080); //commence à accepter les requêtes
